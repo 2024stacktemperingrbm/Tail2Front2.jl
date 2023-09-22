5591483309e608bf3d289a27c9f474df2c7e964d
Jorge FdCD <cossio@users.noreply.github.com>
2023-09-23 01:17:32 +0200

    v1

--- src/Tail2Front2.jl
+++ src/Tail2Front2.jl
@@ -1,5 +1,10 @@
 module Tail2Front2
 
-# Write your package code here.
+export tail2, front2
+
+using Base: tail, front
+
+tail2(t::Tuple) = tail(tail(t))
+front2(t::Tuple) = front(front(t))
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
-using Tail2Front2
-using Test
+import Aqua
+import Tail2Front2
+using Test: @testset, @test, @inferred
+using Tail2Front2: tail2, front2
 
-@testset "Tail2Front2.jl" begin
-    # Write your tests here.
+@testset "aqua" begin
+    Aqua.test_all(Tail2Front2)
+end
+
+@testset "tail2" begin
+    @test @inferred(tail2((1,2,3,4,5))) == (3,4,5)
+    @test @inferred(tail2((1,2))) == ()
+end
+
+@testset "front2" begin
+    @test @inferred(front2((1,2,3,4,5))) == (1,2,3)
+    @test @inferred(front2((1,2))) == ()
 end
